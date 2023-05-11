6a6ea05eb83823025e3e8c921cad9c03b44c1e59
atsushi-12 <121540076+atsushi-12@users.noreply.github.com>
2023-05-11 10:59:54 +0900

    remove import to "rejected file"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
-import Opening from './components/Opening'
-import Three from './components/Three'
-import Menu from "./components/Menu"
-import Test from "./components/Test"
-import { unmountComponentAtNode } from 'react-dom';
-import * as THREE from 'three'
-import React, { Suspense, useEffect, useState,useRef} from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Reflector, Text, useTexture, useGLTF,useVideoTexture,useAspect } from '@react-three/drei'
+import Opening from "./components/Opening";
+import Menu from "./components/Menu";
+import Test from "./components/Test";
+import { unmountComponentAtNode } from "react-dom";
+import * as THREE from "three";
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Reflector,
+  Text,
+  useTexture,
+  useGLTF,
+  useVideoTexture,
+  useAspect,
+} from "@react-three/drei";
 
 export default function Home() {
-
   return (
     <>
-    
-    <Opening/>
-    <Menu/>
-    <Canvas   style={{width:"100vw",height:"100vh"}}concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 20 }}>
-    <Test/>
-    </Canvas>
-    </>  )
+      <Opening />
+      <Menu />
+      <Canvas
+        style={{ width: "100vw", height: "100vh" }}
+        concurrent
+        gl={{ alpha: false }}
+        pixelRatio={[1, 1.5]}
+        camera={{ position: [0, 3, 100], fov: 20 }}
+      >
+        <Test />
+      </Canvas>
+    </>
+  );
 }
